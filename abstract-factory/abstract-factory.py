cd9b66fc92f4767cd7577968e15572303abdc6e0
Peter <peter.j.martinson@gmail.com>
2018-07-26 17:09:56 -0400

    Produce a working factory
    I think this is the *Factory Module* pattern, not the *Abstract Factory*
    pattern!

--- abstract-factory/abstract-factory.py
+++ abstract-factory/abstract-factory.py
@@ -1,29 +1,48 @@
-import abc
-
-
+# The superclass
 class binary_operation(object):
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
     def operate(self, x, y):
-      """Run a binary operation and return the result"""
-      return
+        """Run a binary operation and return the result"""
+        pass
 
 
-
+# The four subclasses
 class Addition(binary_operation):
-    def operate(x, y):
+    def operate(self, x, y):
         return x + y
 
 class Subtraction(binary_operation):
-    def operate(x, y):
+    def operate(self, x, y):
         return x - y
 
 class Multiplication(binary_operation):
-    def operate(x, y):
+    def operate(self, x, y):
         return x * y
 
 class Division(binary_operation):
-    def operate(x, y):
+    def operate(self, x, y):
         return x / y
 
+class BinaryOperationFactory(object):
+    def makeBinaryOperation(self, operator):
+        if operator == '+':
+            return Addition()
+        if operator == '-':
+            return Subtraction()
+        if operator == '*':
+            return Multiplication()
+        if operator == '/':
+            return Division()
+    def getOperator(self, operator):
+        return self.makeBinaryOperation(operator)
+
+factory = BinaryOperationFactory();
+
+add = factory.getOperator('+');
+subtract = factory.getOperator('-');
+multiply = factory.getOperator('*');
+divide = factory.getOperator('/');
+
+print("1 + 1 = ", add.operate(1,1))
+print("5 - 3 = ", subtract.operate(5,3))
+print("3 * 4 = ", multiply.operate(3,4))
+print("17 / 2 = ", divide.operate(17,2))
